f9924547739b94afc0135e97ee1c5ffafbb36b01
PC <lsongulija@gmail.com>
2021-06-22 16:47:43 +0200

    BugsContext replacing some code

--- DataStore.EF/BugsContext.cs
+++ DataStore.EF/BugsContext.cs
@@ -19,7 +19,7 @@ namespace DataStore.EF
          * we will have to provide those options to this constructor 
          * then it'll go to constructor of DbContext class
          */
-        public BugsContext(DbContextOptions options) : base()
+        public BugsContext(DbContextOptions options) : base(options)
         {
         }
         /**
@@ -50,15 +50,15 @@ namespace DataStore.EF
             //seeding. to add initial data to database
             //HasData is array of oobjects
             modelBuilder.Entity<Project>().HasData(
-                new Project { ProjectId = 1, Name = "Project 1"},
+                new Project { ProjectId = 1, Name = "Project 1" },
                 new Project { ProjectId = 2, Name = "Project 2" }
-            );
+                );
             //initial data for tickets table. assigning to different Projects with ProjectId
             modelBuilder.Entity<Ticket>().HasData(
-                new Ticket { TicketId=1, Title ="Bug #1", ProjectId=1 },
+                new Ticket { TicketId = 1, Title = "Bug #1", ProjectId = 1},
                 new Ticket { TicketId = 2, Title = "Bug #2", ProjectId = 1 },
-                new Ticket { TicketId = 2, Title = "Bug #2", ProjectId = 2 }
-            );
+                new Ticket { TicketId = 3, Title = "Bug #3", ProjectId = 2 }
+                );
         }
     }
 }
