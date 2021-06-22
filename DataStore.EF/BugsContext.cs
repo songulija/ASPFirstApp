fb420f5110c059841cfa64e3cae893f6ffd29755
PC <lsongulija@gmail.com>
2021-06-22 16:34:56 +0200

    Added new Project DataStore.EF. Added class BugsContext, configured relationships between tables, added seed data. And configured Startup.cs

---
+++ DataStore.EF/BugsContext.cs
@@ -0,0 +1,64 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataStore.EF
+{
+    /// <summary>
+    /// have to inherit from DbContext. This instance represents session with database and
+    /// can be used to query and save instances of your entities
+    /// BugsContext represents database itself
+    /// </summary>
+
+    public class BugsContext : DbContext
+    {
+        /**
+         * Create a constructor. With DbContextOptions 
+         * these options can be used to configure BugsContext
+         * And be use base class constructor. When using BugsContext class
+         * we will have to provide those options to this constructor 
+         * then it'll go to constructor of DbContext class
+         */
+        public BugsContext(DbContextOptions options) : base()
+        {
+        }
+        /**
+         * Each DbSet corresponds to TABLE. Project and Ticket objects
+         * will have their own tables
+         */
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        /**
+         * After having properties that corresponds to tables
+         * We need to configure database schema. The relationships
+         * between TABLES. WHEN OnModelCreating IS called we are creating 
+         * schema in memory that represents database schema(tables, relationships)
+         * This is method where we configure that schema and table relationships
+         */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //configuring project entity(table) and adding one to many relationship
+            //with Ticket table. Project can have many Tickets, and ticket can have one project
+            //and ticket table has FOREIGN KEY which is Project id
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Tickets)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId);
+
+            //seeding. to add initial data to database
+            //HasData is array of oobjects
+            modelBuilder.Entity<Project>().HasData(
+                new Project { ProjectId = 1, Name = "Project 1"},
+                new Project { ProjectId = 2, Name = "Project 2" }
+            );
+            //initial data for tickets table. assigning to different Projects with ProjectId
+            modelBuilder.Entity<Ticket>().HasData(
+                new Ticket { TicketId=1, Title ="Bug #1", ProjectId=1 },
+                new Ticket { TicketId = 2, Title = "Bug #2", ProjectId = 1 },
+                new Ticket { TicketId = 2, Title = "Bug #2", ProjectId = 2 }
+            );
+        }
+    }
+}
--- FirstAspNet/Startup.cs
+++ FirstAspNet/Startup.cs
@@ -1,7 +1,9 @@
+using DataStore.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,28 +16,52 @@ namespace FirstAspNet
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+        //constructor. To assign 
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            this._env = env;
         }
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         //this configures added dependencies and configures behaviour of middleware
         public void ConfigureServices(IServiceCollection services)
         {
+            //if environment is IsDevelopment use inMemory database
+            if (_env.IsDevelopment())
+            {
+                //we will use in Memory database(LOCAL).
+                //Adding DbContext witch we build.And we are configuring options
+                //and options will be to use InMemoryDatabase. And give NAME to Database
+                //these options will be passed to BugsContext class constructor options
+                services.AddDbContext<BugsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Bugs");
+                });
+            }
+            
+
             //adds services for controllers. basically we configure our added middleware
             //in  Configure endpoints.MapControllers. We dont configure more leavy empty by default
             services.AddControllers();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         //it contains all the middleware you want to add to pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BugsContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                //if IsDevelopment create in-memory database with initial data. becouse
+                //when creating in-memory db you have to have initial data
+                //ensure that database isDeleted and then created
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
             }
             else
             {
