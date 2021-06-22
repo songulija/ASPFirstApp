a6958b604739e89a6317cb57eb45a7516020b726
PC <lsongulija@gmail.com>
2021-06-22 19:08:02 +0200

    Added async and await to all methods of ProjectsController

--- FirstAspNet/Controllers/ProjectsController.cs
+++ FirstAspNet/Controllers/ProjectsController.cs
@@ -36,19 +36,19 @@ namespace FirstAspNet.Controllers
         /// one return type that contains everything. When person makes http GET request to
         /// specified route
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
             //return all data from projects table in our database, AS LIST
-            return Ok(db.Projects.ToList());
+            return Ok(await db.Projects.ToListAsync());
         }
         /**
          * When making GET request with id. Id comes from route to parameter
          */
         [HttpGet("{id}")]
-        public IActionResult GetById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
             //Find method will find entity with that primary key value in Projects table
-            var project = db.Projects.Find(id);
+            var project = await db.Projects.FindAsync(id);
             if (project == null)
                 return NotFound();
 
@@ -64,11 +64,11 @@ namespace FirstAspNet.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("/api/projects/{pid}/tickets")]
-        public IActionResult GetProjectTickets(int pid)
+        public async Task<IActionResult> GetProjectTickets(int pid)
         {
             //In tickets table searching for all tickets where ProjectId
             //is equal to pid that we get from GET request (from route). Get as List
-            var tickets = db.Tickets.Where(t => t.ProjectId == pid).ToList();
+            var tickets = await db.Tickets.Where(t => t.ProjectId == pid).ToListAsync();
             if (tickets == null || tickets.Count <= 0)
                 return NotFound();
 
@@ -82,12 +82,12 @@ namespace FirstAspNet.Controllers
          * Usually we use this in PUT, POST, PATCH methods to provide data
          */
         [HttpPost]
-        public IActionResult Post([FromBody]Project project)
+        public async Task<IActionResult> Post([FromBody]Project project)
         {
             //with Add we add new project to dbContext, it will be marked as added
             //and then with SaveChanges we basically insert it to db
             db.Projects.Add(project);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
 
             //this CreatedAtAction will return 201 code which means created
             //providing action(function) name GetById providing that function id it needs
@@ -105,7 +105,7 @@ namespace FirstAspNet.Controllers
          * Usually we use this in PUT, POST, PATCH methods to provide data
          */
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] Project project)
+        public async Task<IActionResult> Put(int id, [FromBody] Project project)
         {
             if (id != project.ProjectId) return BadRequest();
 
@@ -115,11 +115,11 @@ namespace FirstAspNet.Controllers
             //try if saveChanges fails then. 
             try
             {
-                db.SaveChanges();
+                await db.SaveChangesAsync();
             }
             catch
             {
-                if (db.Projects.Find(id) == null)
+                if (await db.Projects.FindAsync(id) == null)
                     return NotFound();
                 throw;
             }
@@ -132,16 +132,16 @@ namespace FirstAspNet.Controllers
          * Id comes from route to parameter
          */
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             //you can implement like hide field to not actually delete
-            var project = db.Projects.Find(id);
+            var project = await db.Projects.FindAsync(id);
             if (project == null) 
                 return NotFound();
             //remove from dbContext, it will mark as deleted, and SaveChanges
             //will generate actual delete statement to delete from DB
             db.Projects.Remove(project);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
 
             return Ok(project);
         }
