9183012fa63454e52b0984f64db7ce795634e435
PC <lsongulija@gmail.com>
2021-06-24 19:02:25 +0200

    Added GetTicket,Create,update,delete tickets methods to Program.cs

--- App.Testing.Console/Program.cs
+++ App.Testing.Console/Program.cs
@@ -89,3 +89,77 @@ async Task DeleteProject(int id)
     ProjectRepository repository = new(apiExecuter);
     await repository.DeleteAsync(id);
 }
+
+async Task TestTickets()
+{
+    Console.WriteLine("////////////////////");
+    Console.WriteLine("Reading all tickets...");
+    await GetTickets();
+
+    Console.WriteLine("////////////////////");
+    Console.WriteLine("Reading contains 1...");
+    await GetTickets("1");
+
+    Console.WriteLine("////////////////////");
+    Console.WriteLine("Create a ticket...");
+    var tId = await CreateTicket();
+    await GetTickets();
+
+    Console.WriteLine("////////////////////");
+    Console.WriteLine("Update a ticket...");
+    var ticket = await GetTicketById(tId);
+    await UpdateTicket(ticket);
+    await GetTickets();
+
+    Console.WriteLine("////////////////////");
+    Console.WriteLine("Delete a ticket...");
+    await DeleteTicket(tId);
+    await GetTickets();
+}
+
+async Task GetTickets(string filter = null)
+{
+    //GET all tickets with repository help
+    TicketRepository ticketRepository = new(apiExecuter);
+    var tickets = await ticketRepository.GetAsync(filter);
+    foreach (var ticket in tickets)
+    {
+        Console.WriteLine($"Ticket: {ticket.Title}");
+    }
+}
+
+async Task<Ticket> GetTicketById(int id)
+{
+    //GET ticket by id with repository help
+    TicketRepository ticketRepository = new(apiExecuter);
+    var ticket = await ticketRepository.GetByIdAsync(id);
+    return ticket;
+}
+
+async Task<int> CreateTicket()
+{
+    //Create new ticket with repository help
+    TicketRepository ticketRepository = new(apiExecuter);
+    return await ticketRepository.CreateAsync(new Ticket
+    {
+        ProjectId = 2,
+        Title = "This a very difficult.",
+        Description = "Something is wrong on the server."
+    });
+}
+
+async Task UpdateTicket(Ticket ticket)
+{
+    //Update ticket with repository help
+    TicketRepository ticketRepository = new(apiExecuter);
+    ticket.Title += " Updated";
+    await ticketRepository.UpdateAsync(ticket);
+}
+
+async Task DeleteTicket(int id)
+{
+    //Delete ticket with repository help
+    TicketRepository ticketRepository = new(apiExecuter);
+    await ticketRepository.DeleteAsync(id);
+}
+
